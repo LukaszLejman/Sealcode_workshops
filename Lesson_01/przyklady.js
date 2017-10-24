22206734ef8291524030577a7e4d34e8fbe0faf6
LukaszLejman <LukaszLejman96@wp.pl>
2017-10-24 12:16:21 +0200

    121

--- Lesson_01/przyklady.js
+++ Lesson_01/przyklady.js
@@ -1,5 +1,26 @@
-for (let i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 100);
+/* Zasięg funkcyjny */
+
+var i = 100; // zmienna globalna
+for (var i = 0; i < 5; i++) { // nadpisanie tej samej zmiennej globalnej
+    // działanie pętli for
 }
+console.log(i); // wyświetli wartość 5
+
+/* Domknięcie */
+
+var j = 100;
+(function () {
+    for (var j = 0; j < 5; j++) { // zmiennna lokalna
+        // działanie pętli for
+    }
+})();
+console.log(j); // wyświetli wartość 100
+
+/* Zasięg blokowy */
+
+let k = 100; // zmienna globalna
+for (let k = 0; k < 5; k++) { // zmiennna lokalna w zakresie pętli for
+    // działanie pętli for
+}
+
+console.log(k); // wyświetli wartość 100
