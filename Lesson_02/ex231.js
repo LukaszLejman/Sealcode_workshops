2bb8d25f784def73d1c829d43e151e8e70145325
LukaszLejman <LukaszLejman96@wp.pl>
2017-11-14 12:20:21 +0100

    ex231.js

---
+++ Lesson_02/ex231.js
@@ -0,0 +1,20 @@
+let a = $('h1');
+console.log(a);
+console.log(a.html());
+console.log(a.text());
+
+let b = $('aside h2');
+console.log(b);
+console.log(b.html());
+console.log(b.text());
+
+let c = $('aside section:last-child p');
+console.log(c);
+console.log(c.html());
+console.log(c.text());
+
+let d = $('form fieldset:nth-child(3)');
+console.log(d);
+console.log(d.html());
+console.log(d.text());
+
